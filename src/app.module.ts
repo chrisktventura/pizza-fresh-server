95072861afb5303d97d293591e8502824f154b7f
Christopher Ventura <chrisventura@outlook.com.br>
2022-05-15 10:16:17 -0300

    create table module & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { TableModule } from './table/table.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TableModule],
   controllers: [AppController],
   providers: [AppService],
 })
