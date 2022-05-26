0ac14b9662769f31957a10dc55d8fe92c7c1e57d
Christopher Ventura <chrisventura@outlook.com.br>
2022-05-26 20:17:30 -0300

    up

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do produto',
+    example: 'Pizza de Calabresa',
+  })
   name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Descrição do produto',
+    example: 'Pizza de calabresa com cebola e queijo',
+  })
   description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: 'Preço do produto',
+    example: '25.00',
+  })
   price: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do produto',
+    example: 'https://i.imgur.com/hNE75Iw.png',
+  })
   image: string;
 }
