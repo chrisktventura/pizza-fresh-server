78f62246bc62a49248644f6f18c453bef60e3c0c
Christopher Ventura <chrisventura@outlook.com.br>
2022-05-30 20:31:36 -0300

    atualizando rotas de status

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
 
   // Validation
   app.useGlobalPipes(new ValidationPipe());
+
+  app.set('trust proxy', 1);
 
   // Swagger
   const config = new DocumentBuilder()
